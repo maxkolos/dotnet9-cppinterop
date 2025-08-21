ac9cb972c86c82d8de477fd3f4d4def1422fe040
maxkolos <99802038+maxkolos@users.noreply.github.com>
2025-08-21 06:14:49 +0000

    Fix Docker smoke test flakiness

--- src/tests/run_all_tests.sh
+++ src/tests/run_all_tests.sh
@@ -3,7 +3,6 @@ set -x
 
 cd CppLibraryTests && sh run_unittests.sh && sh run_fuzz_tests.sh && cd .. && \
 cd StringService.Tests && sh run_StringServiceTests.sh && cd .. && \
-# Note: Docker test rarely flakes when running with many other tests.
 cd DockerSmokeTest && sh run_docker_smoke_test.sh && cd .. && \
 cd web-ui.Tests && sh run_tests.sh && cd .. && \
 cd BrowserTests && sh run_browser_tests.sh && cd .. && echo "All tests passed"
